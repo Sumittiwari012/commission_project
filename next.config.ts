825e4f7b4e15dc8a59c49afe0feca273a7e3595a
Sumittiwari012 <st386700@gmail.com>
2026-01-25 09:40:53 +0530

    ui updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["img.sanishtech.com"],
+    unoptimized: true // for Netlify
+  }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
